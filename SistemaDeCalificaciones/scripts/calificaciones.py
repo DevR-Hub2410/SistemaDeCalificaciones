d691928585aca0a1f48c50181caf1a81828622be
DevR-Hub2410 <r.castillov4@gmail.com>
2026-05-04 13:00:19 -0600

    Registro de nombres de alumnos

--- SistemaDeCalificaciones/scripts/calificaciones.py
+++ SistemaDeCalificaciones/scripts/calificaciones.py
@@ -1,2 +1,10 @@
+nombres = [None for _ in range(20)]
+calificaciones = [[0 for _ in range(20)] for _ in range(5)]
+
 def init():
-    print("Metodo principal de calificaciones")
+    registrarAlumnos()
+    
+def registrarAlumnos():
+    for i in range(20):
+        nom = input("Ingresa el nombre del alumno: ")
+        nombres[i] = nom
