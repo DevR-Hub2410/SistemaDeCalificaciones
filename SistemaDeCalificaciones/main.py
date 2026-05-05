bb664f2c2ae0d6b7a788aae536e7e47ddb0de100
DevR-Hub2410 <r.castillov4@gmail.com>
2026-05-05 00:00:16 -0600

    Mejora de registro de alumnos y calificaciones

--- SistemaDeCalificaciones/main.py
+++ SistemaDeCalificaciones/main.py
@@ -1,14 +1,17 @@
+import os
+
 import scripts.calificaciones as cal
 import scripts.estatus as est
 import scripts.reportes as rep
 
 def menu():
-    print("-----Sistema de Calificaciones-----")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("\033[32m-----Sistema de Calificaciones-----\033[0m")
     print("a) Almacenar datos del alumno")
     print("b) Ver estatus del alumno")
     print("c) Extraer reporte")
     print("d) Terminar el programa")
-    opcion = input("")
+    opcion = input(">> ")
     
     if opcion == "a":
         cal.init()
--- SistemaDeCalificaciones/scripts/calificaciones.py
+++ SistemaDeCalificaciones/scripts/calificaciones.py
@@ -1,10 +1,25 @@
-nombres = [None for _ in range(20)]
-calificaciones = [[0 for _ in range(20)] for _ in range(5)]
+import os
+
+nombres = [None for _ in range(6)]
+calificaciones = [[0 for _ in range(5)] for _ in range(6)]
 
 def init():
+    os.system('cls' if os.name == 'nt' else 'clear')
     registrarAlumnos()
+    registrarCalificaciones()
     
 def registrarAlumnos():
-    for i in range(20):
+    for i in range(5):
         nom = input("Ingresa el nombre del alumno: ")
         nombres[i] = nom
+        os.system('cls' if os.name == 'nt' else 'clear')
+        
+def registrarCalificaciones():
+    for i in range(20):
+        for j in range(5):
+            calif = float(input(f"Ingresa la calificacion del alumno {nombres[i]}: "))
+            calificaciones[i][j] = calif
+            os.system('cls' if os.name == 'nt' else 'clear')
+    # for i in calificaciones:
+        # print(i, end=" ")
+    print("Fin de la ejecucion")
